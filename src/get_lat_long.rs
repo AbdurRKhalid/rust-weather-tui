61b792c89904064a387c528dbefca46fc661bfa5
Abdur Rehman Khalid <abdur.khalid@allex.ai>
2025-12-26 00:00:09 +0100

    Corrected the API request format and removed all unused variables

--- src/get_lat_long.rs
+++ src/get_lat_long.rs
@@ -1,7 +1,8 @@
 use reqwest;
 
-pub async fn get_latitude_longitude(cityName: &String, applicationKey: &str) -> Result<String, Box<dyn std::error::Error>> {
-    let url = format!("http://api.openweathermap.org/geo/1.0/direct?q={}&limit=1&appid={}", cityName, applicationKey);
+pub async fn get_latitude_longitude(city_name: &String, application_key: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let url = format!("http://api.openweathermap.org/geo/1.0/direct?q={}&limit=1&appid={}", city_name, application_key);
+    println!("{}", url);
     let response = reqwest::get(url).await?;
     let body = response.text().await?;
     Ok(body)
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ use get_lat_long::get_latitude_longitude;
 async fn main() {
     let args: Vec<String> = env::args().collect();
     let city = &args[1];
-    let weather_type = &args[3];
     let application_key = "a25e026039a4ee20f206ce5b9bb1b59b";
 
     match get_latitude_longitude(city, application_key).await{
