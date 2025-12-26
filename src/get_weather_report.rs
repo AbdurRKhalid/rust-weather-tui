58736c69d0ee9fbea9ceafe50ccab545696ce443
Abdur Rehman Khalid <abdur.khalid@allex.ai>
2025-12-27 00:40:18 +0100

    printed the basic info in formatted format

--- src/get_weather_report.rs
+++ src/get_weather_report.rs
@@ -2,8 +2,7 @@ use reqwest;
 use serde_json::Value;
 
 pub async fn get_simple_weather(latitude: f64, longitude: f64, application_key: &str) -> Result <Value, Box<dyn std::error::Error>> {
-    println!("{},{}", latitude, longitude);
-    let url = format!("http://api.openweathermap.org/data/2.5/weather?lat={}&lon{}&appid={}", latitude, longitude, application_key);
+    let url = format!("http://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}&units=metric", latitude, longitude, application_key);
     let response = reqwest::get(url).await?;
     let json: Value = response.json().await?;
     Ok(json)
--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,30 @@ async fn main() {
     if weather_type == "normal" {
         match get_simple_weather(latitude, longitude, application_key).await {
             Ok(json) => {
-                println!("{}", json);
+                let weather_data = &json;
+                let current_temp = &weather_data["main"]["temp"];
+                let feels_like_temp = &weather_data["main"]["feels_like"];
+                let min_temp = &weather_data["main"]["temp_min"];
+                let max_temp = &weather_data["main"]["temp_max"];
+                let humidity = &weather_data["main"]["humidity"];
+                let main_weather_type = &weather_data["weather"][0]["main"];
+                let wind_speed = &weather_data["wind"]["speed"];
+                println!("look outside window it is {}", main_weather_type);
+                if main_weather_type == "Mist" {
+                    println!("\x1b[90m     ___  \x1b[0m");
+                    println!("\x1b[90m  _(   )_\x1b[0m");
+                    println!("\x1b[90m (       )\x1b[0m");
+                    println!("\x1b[90m  ~~~~~~~\x1b[0m");
+                    println!("\x1b[90m   Mist\x1b[0m");
+                }
+                println!("\n{:<20} | {}", "Property", "Value");
+                println!("{:-<20}-+-{:-<15}", "", "");
+                println!("{:<20} | {}", "Temperature", format!("{}°C",current_temp));
+                println!("{:<20} | {}", "Feels Like", format!("{}°C", feels_like_temp));
+                println!("{:<20} | {}", "Min Temperature", format!("{}°C", min_temp));
+                println!("{:<20} | {}", "Max Temperature", format!("{}°C", max_temp));
+                println!("{:<20} | {}", "Humidity", format!("{}%", humidity));
+                println!("{:<20} | {}", "Wind Speed", format!("{} m/s", wind_speed));
             }
             Err(e) => eprintln!("Error :{}", e)
         }
